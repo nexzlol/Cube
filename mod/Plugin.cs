002f7c19c41da66dfac24bcbcb4c03bc5d3320dc
Shiny003 <dextermorley27@gmail.com>
2025-05-31 19:20:00 -0600

    fixed finally

--- mod/Plugin.cs
+++ mod/Plugin.cs
@@ -1,20 +1,20 @@
 ﻿using BepInEx;
-using Photon.Pun;
 using UnityEngine;
-namespace mod
+
+namespace modFixed;
+
+[BepInPlugin("nexz.Mod", "Mod",  "1.0.0")]
+public class Plugin : BaseUnityPlugin
 {
-    [BepInPlugin("mod","mod","1.0.0")]
-    public class Plugin : BaseUnityPlugin
+    public void Awake()
     {
-        public void Awake()
-        {
-            GorillaTagger.OnPlayerSpawned(PlayerSpawned);
-        }
-        public void PlayerSpawned()
-        {
-            var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            obj.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
-            obj.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
-        }
+        GorillaTagger.OnPlayerSpawned(PlayerSpawned);
+    }
+
+    public void PlayerSpawned()
+    {
+        var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        obj.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
+        obj.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
     }
 }
