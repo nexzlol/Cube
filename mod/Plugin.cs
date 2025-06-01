a4309d3b7926a3f9ac3415a2a7db35d1070213b0
abram <abram@ALIEN>
2025-05-31 20:47:02 -0400

    Add project files.

---
+++ mod/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using Photon.Pun;
+using UnityEngine;
+namespace mod
+{
+    [BepInPlugin("mod","mod","1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            GorillaTagger.OnPlayerSpawned(PlayerSpawned);
+        }
+        public void PlayerSpawned()
+        {
+            var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            obj.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
+            obj.transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+        }
+    }
+}
